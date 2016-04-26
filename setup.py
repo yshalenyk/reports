e36a897645ce7ccd13fd1c8c1b760c0210a99ab0
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-04-26 13:36:53 +0300

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import sys
+from setuptools import setup
+
+install_requires = [
+    'couchdbkit>=0.6.5',
+    'dateparser>=0.3.4',
+]
+
+
+setup(
+    name='reports',
+    version='0.0.1',
+    packages=[
+        'reports',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bids = reports.bids:run',
+            'tenders = reports.tenders:run',
+            'refunds = reports.refunds:run',
+            'invoices = reports.invoices:run',
+        ]
+    },
+    install_requires=install_requires
+)
