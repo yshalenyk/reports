f1160acc4c5aebf8b563d1b9885bef2d0fdb5f1f
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-05-11 13:46:51 +0300

    modified setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'bids = reports.bids:run',
-            'tenders = reports.tenders:run',
-            'refunds = reports.refunds:run',
-            'invoices = reports.invoices:run',
+            'bids = reports.utilities.bids:run',
+            'tenders = reports.utilities.tenders:run',
+            'refunds = reports.utilities.refunds:run',
+            'invoices = reports.utilities.invoices:run',
             'init = reports.db_init:run',
         ]
     },
