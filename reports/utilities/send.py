254ef3af2a43aaec60b91656d37bcc2516babd40
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2016-07-25 15:04:39 +0300

    Added send to s3 script

---
+++ reports/utilities/send.py
@@ -0,0 +1,85 @@
+import boto3
+import argparse
+import os.path
+import logging
+import os
+from gevent.pool import Pool
+from botocore.exceptions import ClientError
+from logging.config import fileConfig
+from ConfigParser import ConfigParser
+
+Logger = None
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--config',
+        dest='config',
+        required=True,
+        help='Path to configuration file'
+    )
+    parser.add_argument(
+        '-f',
+        '--file',
+        nargs='+',
+        required=True,
+        dest='files',
+        help='Files to send'
+    )
+    parser.add_argument(
+        '-n',
+        '--notify',
+        action='store_true',
+        help='Notification flag'
+    )
+    return parser
+
+
+class AWSClient(object):
+
+    def __init__(self, config):
+        fileConfig(config)
+        try:
+            self.session = boto3.Session()
+        except ClientError as e:
+            print "Errori {}".format(e)
+        self.Logger = logging.getLogger('AWS')
+        self.expires = config.get('aws', 'bucket')
+        self.bucket = config.get('aws', 'expires')
+        self.s3 = self.session.client('s3')
+        self.links = {}
+
+    def send_file(self, file):
+        broker = file.split('@')[0]
+        key = os.path.basename(file)
+        try:
+            self.s3.upload_file(
+                    file,
+                    self.bucket,
+                    key)
+            self.links[broker] = self.s3.generate_presignet_url(
+                'get_object',
+                Params={'Bucket': self.bucket, 'Key': key},
+                ExpiresIn=self.expires,
+            )
+        except ClientError as e:
+            print "Error during uploading file {}. Error {}".format(file, e)
+
+
+def run():
+    parser = get_parser()
+    args = parser.parse_args()
+    config = ConfigParser()
+    config.read(args.config)
+
+    client = AWSClient(config)
+    pool = Pool(10)
+    pool.map(client.send_file, args.files)
+    for key, link in client.links.iteritems():
+        print "Url for {} ==>{}\n".format(key, link)
+
+
+if __name__ == '__main__':
+    run()
