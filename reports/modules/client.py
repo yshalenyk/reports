1467dde357a865049345d470c07295e829ca1e5e
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-02-03 19:02:23 +0200

    Add vault to retreive secrets from pass

---
+++ reports/modules/client.py
@@ -0,0 +1,25 @@
+import subprocess
+
+
+class PassClient(object):
+
+    def __init__(self):
+        self.cmd = 'pass'
+
+    def _output(self, cmd):
+        return subprocess.check_output(cmd, shell=True)
+
+    def _credentials(self, path):
+        cmd = self.cmd + ' {}'.format(path)
+        out = self._output(cmd)
+        return dict(item.split('=') for item
+                    in out.split('\n') if item)
+
+    def ses_credentials(self, key):
+        self._credentials(key)
+
+    def s3_credentials(self, key):
+        self._credentials(key)
+
+    def broker_password(self, key):
+        return self._output(self.cmd + ' {}'.format(key))
---
+++ reports/vault.py
@@ -0,0 +1,21 @@
+from reports.modules import PassClient
+from os.path import join
+
+
+class Vault(object):
+
+    def __init__(self, config):
+        self.type = config.vault
+        if self.type != 'pass':
+            raise NotImplementedError('Only pass is supported')
+        self.config = config
+        self.client = PassClient()
+
+    def ses(self):
+        return self.client.ses_credentials(self.config.ses)
+
+    def s3(self):
+        return self.client.s3_credentials(self.config.s3)
+
+    def broker_password(self, broker):
+        return self.client.broker_password(join(self.config.zip, broker))
